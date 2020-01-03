2a66bd957e0f2ce1eb82f89d84b6121f74cfb333
itachi00000 <>
2020-01-03 22:10:46 +0800

    create detect image project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecognition /> */}
+    </>
+  );
+}
+
+export default App;
