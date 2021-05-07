0ec2fab3adab5653f26785c256ea72a6ececc560
itachi00000 <>
2021-05-07 21:13:52 +0800

    update react-scripts to v4.xx, remove service worker

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'tachyons';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
